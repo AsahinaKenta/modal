ac1b2e05736ff4b42e991f1d5c38aff95cd04d80
AsahinaKenta <0eb0r1517875j0a@gmail.com>
2018-04-07 13:38:42 +0900

    ボタン追加

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var modal = document.getElementById('modal');
 var modalContent = document.getElementById('modal-content');
 var modalImg = document.getElementById('modal-img');
 var imgLists = document.getElementById('img-lists');
+var prevBtn = document.getElementById('prev-btn');
+var nextBtn = document.getElementById('next-btn');
+
 console.dir(imgLists);
 console.dir(imgLists.children[0]);
 
@@ -18,6 +21,24 @@ for (var i = 0; i < imgLists.children.length; i++) {
     }
   })(i));
 }
+// for (var i = 0; i < imgLists.children.length; i++) {
+//   imgLists.children[i].addEventListener('click', (function(index){
+//     return function(){
+//       // console.dir(imgLists);
+//       // console.dir(imgLists.children[i]);
+//       console.log(index);
+//       var imgSrc = imgLists.children[index].children[0].src;
+//       var imgSrcArray = imgSrc.split('/');
+//       console.log(imgSrc);
+//       console.log(imgSrcArray);
+//       var imgName = imgSrcArray[imgSrcArray.length - 1];
+//       console.log(imgName);
+//       var imgPath = './img/' + imgName;
+//       modalImg.setAttribute('src', imgSrc);
+//       modal.classList.add('active');
+//     }
+//   })(i));
+// }
 
 // clickImg.addEventListener('click', function(){
 //   console.log(clickImg.src);
@@ -33,3 +54,51 @@ modal.addEventListener('click', function(){
 modalContent.addEventListener('click',function(e){
   e.stopPropagation();
 });
+
+prevBtn.addEventListener('click',function(e){
+  console.log('prevボタンをクリック');
+  e.preventDefault();
+  // var currentImgSrc = modalImg.getAttribute('src');
+  // var currentImgSrcNum = +currentImgSrc.split('/')[currentImgSrc.split('/').length - 1].substr(-5, 1);
+  // currentImgSrcNum--;
+  // if(currentImgSrcNum === 0){
+  //   currentImgSrcNum = 3;
+  // }
+  // var prevImgSrc = './img/enemy-0' + currentImgSrcNum + '.png';
+  // modalImg.setAttribute('src', prevImgSrc);
+  changeImg(e);
+});
+
+nextBtn.addEventListener('click',function(e){
+  console.log('nextボタンをクリック');
+  e.preventDefault();
+  // var currentImgSrc = modalImg.getAttribute('src');
+  // var currentImgSrcNum = +currentImgSrc.split('/')[currentImgSrc.split('/').length - 1].substr(-5, 1);
+  // currentImgSrcNum++;
+  // if(currentImgSrcNum === 4){
+  //   currentImgSrcNum = 1;
+  // }
+  // var nextImgSrc = './img/enemy-0' + currentImgSrcNum + '.png';
+  // modalImg.setAttribute('src', nextImgSrc);
+  changeImg(e);
+  // console.log(currentImgSrc.split('/')[2].substr(-5, 1));
+});
+
+function changeImg(e){
+  var currentImgSrc = modalImg.getAttribute('src');
+  var currentImgSrcNum = +currentImgSrc.split('/')[currentImgSrc.split('/').length - 1].substr(-5, 1);
+  if(e.target.classList.contains('fa-caret-right')){
+    currentImgSrcNum++;
+    if(currentImgSrcNum === 4){
+      currentImgSrcNum = 1
+    }
+  }else{
+    currentImgSrcNum--;
+    if(currentImgSrcNum === 0){
+      currentImgSrcNum = 3;
+    }
+  }
+
+  var nextImgSrc = './img/enemy-0' + currentImgSrcNum + '.png';
+  modalImg.setAttribute('src', nextImgSrc);
+}
--- study.js
+++ study.js
@@ -1,8 +1,19 @@
 
 // 即時関数
 
-var result = (function(){
-    var base =10;
-    var result = base + 5;
-    return sum;
-})();
+// var result = (function(){
+//     var base =10;
+//     var result = base + 5;
+//     return sum;
+// })();
+// グローバルスコープ
+// var x = 10;
+//
+// function log(){
+//   ローカルスコープ（ここでしか使えない変数）
+//   var y = 10;
+//   console.log(x);
+// }
+// log();
+//
+// console.log(y);
